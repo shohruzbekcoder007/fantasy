334a291302b67a125dad652a626d16036d6711c4
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-01-15 00:19:35 +0300

    login started

--- startup/router.js
+++ startup/router.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const starter_router = require('./../routers/starter_router')
-
+const config = require('config')
+const cookieParser = require("cookie-parser")
 
 const corsOptions = {
     exposedHeaders: "x-auth-token",
@@ -11,6 +12,10 @@ const corsOptions = {
 module.exports = (app) => {
     app.use(express.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    config.get('jwtPrivateKey')
     app.use(cors(corsOptions));
+    app.use(cookieParser('q1y1npar0l'));
+    app.use(express.static('./static'));
+    app.set("view engine", "pug");
     app.use('/', starter_router)
 }
